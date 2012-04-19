0826cc5ad2cba99a265c6c33e6889381fd799bc3
Victor ADASCALITEI <adascalitei.victor@gmail.com>
2012-04-20 00:31:01 +0300

    Organized the code a little by making the 'RemRep' class.

--- src/crep/main.cpp
+++ src/crep/main.cpp
@@ -3,28 +3,51 @@
 #include "mongoose/mongoose.h"
 #include "cJSON/cJSON.h"
 
-void *callback(enum mg_event event,
-	       struct mg_connection *conn,
-	       const struct mg_request_info *request_info) {
-  if (event == MG_NEW_REQUEST) {
-    cJSON *root = cJSON_CreateObject();
-    cJSON_AddItemToObject(root, "name", cJSON_CreateString("Jack (\"Bee\") Nimble"));
-    mg_printf(conn, "HTTP/1.1 200 OK\r\n"
+class RemRep {
+public:
+  RemRep(const std::string &lports) : mListenPorts(lports) {}
+  ~RemRep() {
+    StopRepServer();
+  }
+
+  void StartRepServer() {
+    const char *options[] = {"listening_ports", mListenPorts.c_str(), NULL};
+
+    mCtx = mg_start(&WebCallback, NULL, options);
+  }
+  void StopRepServer() {
+    mg_stop(mCtx);
+  }
+  
+protected:
+  std::string mListenPorts;
+  struct mg_context *mCtx;
+
+  static void *WebCallback(enum mg_event event, struct mg_connection *conn, const struct mg_request_info *request_info) {
+    cJSON *root = nullptr;
+    
+    switch(event) {
+    case MG_NEW_REQUEST:
+      root = cJSON_CreateObject();
+      cJSON_AddItemToObject(root, "name", cJSON_CreateString("Vic cel mic!"));
+      mg_printf(conn, "HTTP/1.1 200 OK\r\n"
               "Content-Type: text/plain\r\n\r\n"
               "%s", cJSON_Print(root));//request_info->uri);
-    return (void *)"";  // Mark as processed
-  } else {
+      return (void *)"";  // Mark as processed
+      break;
+    default:
+      break;
+    }
+
     return NULL;
   }
-}
+};
 
 int main(int argc, char *argv[]) {
-  struct mg_context *ctx;
-  const char *options[] = {"listening_ports", "8080", NULL};
+  RemRep *rep = new RemRep("8080");
 
-  ctx = mg_start(&callback, NULL, options);
+  rep->StartRepServer();
   getchar();
-  mg_stop(ctx);
-
+  delete rep;
   return 0;
 }
