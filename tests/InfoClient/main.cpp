98630137180c34a77bcc1536c60c24febce49991
Victor ADASCALITEI <adascalitei.victor@gmail.com>
2012-05-05 11:20:24 +0300

    Started building client code to work with the 'InfoServer' test.

---
+++ tests/InfoClient/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include "rcos_InfoServer.cpp"
+
+int main(int argc, char *argv[]) {
+  InfoServer rcosInfo = InfoServer.getInstance("127.0.0.1:8081");
+
+  std::cout << "Informatii despre drumul 0 : " << rcosInfo.getRoadInfo(0) << std::endl;
+  std::cout << "Temperatura pentru Timisoara : " << rcosInfo.getTemp("Timisoara") << std::endl;
+
+  return 0;
+}
